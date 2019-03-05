4c3a9766b9efb9dd4d35c9f142dd3c834f40de99
Will Axtell <will.axtell@natl.com>
2019-03-05 14:21:47 -0500

    Incremental commit - Refactored batch processor to facilitate more customization

--- Aop.Profiler/EventProcessing/EventBatchProcessor.cs
+++ Aop.Profiler/EventProcessing/EventBatchProcessor.cs
@@ -1,140 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Aop.Profiler.EventProcessing
 {
-    public class EventBatchProcessor : IProcessProfilerEvents, IDisposable
+    public class EventBatchProcessor : EventBatchProcessorBase
     {
-        private readonly int _batchSizeLimit;
-        private readonly TimeSpan _period;
-        private readonly BoundedConcurrentQueue<IDictionary<string,object>> _queue;
-        private readonly Queue<IDictionary<string, object>> _waitingBatch = new Queue<IDictionary<string, object>>();
-        private readonly object _stateLock = new object();
-        private readonly WakeTimer _timer;
-        private bool _unloading;
         private readonly Func<Queue<IDictionary<string, object>>, Task> _processEvents;
 
         public EventBatchProcessor(Func<Queue<IDictionary<string, object>>, Task> processEvents, int batchSizeLimit, TimeSpan period, uint queueLimit)
+        : base(batchSizeLimit,period,queueLimit)
         {
             _processEvents = processEvents;
-            _batchSizeLimit = batchSizeLimit;
-            _timer = new WakeTimer(cancel => ProcessBatch());
-            _period = period;
-            _queue = new BoundedConcurrentQueue<IDictionary<string,object>>(queueLimit);
-
-            _timer.Start(period);
         }
 
-        private void CloseAndFlush()
+        public override Task ProcessEvents(Queue<IDictionary<string, object>> events)
         {
-            lock (_stateLock)
-            {
-                if (_unloading)
-                {
-                    return;
-                }
-
-                _unloading = true;
-            }
-
-            _timer.Dispose();
-
-            ResetSyncContextAndWait(ProcessBatch);
-        }
-
-        private static void ResetSyncContextAndWait(Func<Task> taskFactory)
-        {
-            var prevContext = SynchronizationContext.Current;
-            SynchronizationContext.SetSynchronizationContext(null);
-
-            try
-            {
-                taskFactory().Wait();
-            }
-            finally
-            {
-                SynchronizationContext.SetSynchronizationContext(prevContext);
-            }
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposing)
-            {
-                return;
-            }
-
-            CloseAndFlush();
-        }
-
-        private async Task ProcessBatch()
-        {
-            try
-            {
-                bool wasFullBatch;
-
-                do
-                {
-                    while (_waitingBatch.Count < _batchSizeLimit && _queue.TryDequeue(out var next))
-                    {
-                        _waitingBatch.Enqueue(next);
-                    }
-
-                    if (_waitingBatch.Count == 0)
-                    {
-                        return;
-                    }
-
-                    await _processEvents(_waitingBatch);
-
-                    wasFullBatch = _waitingBatch.Count >= _batchSizeLimit;
-
-                    _waitingBatch.Clear();
-                }
-                while (wasFullBatch);
-            }
-            // ReSharper disable once EmptyGeneralCatchClause
-            catch
-            {
-            }
-            finally
-            {
-                lock (_stateLock)
-                {
-                    if (!_unloading)
-                    {
-                        SetTimer(_period);
-                    }
-                }
-            }
-        }
-
-        private void SetTimer(TimeSpan interval)
-        {
-            _timer.Start(interval);
-        }
-
-        public void ProcessEvent(IDictionary<string,object> @event)
-        {
-            if (@event == null)
-            {
-                throw new ArgumentNullException(nameof(@event));
-            }
-
-            // ReSharper disable once InconsistentlySynchronizedField
-            if (_unloading)
-            {
-                return;
-            }
-
-            _queue.TryEnqueue(@event);
+            return _processEvents(events);
         }
     }
 }
--- Aop.Profiler/IPerMethodAdapter.cs
+++ Aop.Profiler/IPerMethodAdapter.cs
@@ -6,9 +6,9 @@ namespace Aop.Profiler
 {
     public interface IPerMethodAdapter<T> : IProfilerAdapter<T> where T : class
     {
-        IPerMethodAdapter<T> Profile<TReturn>(Expression<Func<T, Task<TReturn>>> target, CaptureOptions captureOptions);
-        IPerMethodAdapter<T> Profile(Expression<Func<T, Task>> target, CaptureOptions captureOptions);
-        IPerMethodAdapter<T> Profile(Expression<Action<T>> target, CaptureOptions captureOptions);
-        IPerMethodAdapter<T> Profile<TReturn>(Expression<Func<T, TReturn>> target, CaptureOptions captureOptions);
+        IPerMethodAdapter<T> Profile<TReturn>(Expression<Func<T, Task<TReturn>>> target, CaptureOptions captureOptions = CaptureOptions.Default);
+        IPerMethodAdapter<T> Profile(Expression<Func<T, Task>> target, CaptureOptions captureOptions = CaptureOptions.Default);
+        IPerMethodAdapter<T> Profile(Expression<Action<T>> target, CaptureOptions captureOptions = CaptureOptions.Default);
+        IPerMethodAdapter<T> Profile<TReturn>(Expression<Func<T, TReturn>> target, CaptureOptions captureOptions = CaptureOptions.Default);
     }
 }
